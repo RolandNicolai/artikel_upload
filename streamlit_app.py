b8eab560b7ece18a97547fde101e2f79b7d72e1a
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 15:15:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,37 @@
 import streamlit as st
-st.title("Google Auth demo!")
 
-if not st.experimental_user.is_logged_in():
-    st.warning("User not logged in")
+st.title("Auth demo!")
 
-google_button = st.button ("Login with Google", type="primary")
 
-if google_button:
-    st. experimental_user.login (provider="google")
+with st.echo():
+    st.write("Is user logged in?", st.experimental_user.is_logged_in())
 
-if st.experimental_user.is_logged_in():
-    st.write(": sparkles: :rainbow[User data]")
+left, middle, right, logout_button_column = st.columns(4)
 
-st.write(st. experimental_user)
-logout_button = st.button ("Logout")
 
-if logout_button:
-    st.experimental_user.logout()
+with left:
+    google_button = st.button("Login with Google")
+
+    if google_button:
+        st.experimental_user.login(provider="google")
+
+with middle:
+    auth_zero_login = st.button("Auth0 Login")
+    if auth_zero_login:
+        st.experimental_user.login(provider="auth0")
+
+with right:
+    microsoft_login = st.button("Microsoft Login")
+
+    if microsoft_login:
+        st.experimental_user.login(provider="microsoft")
+
+
+st.write(":sparkles: :rainbow[User data]")
+st.write(st.experimental_user)
+
+
+with logout_button_column:
+    logout_button = st.button("Logout")
+    if logout_button:
+        st.experimental_user.logout()
