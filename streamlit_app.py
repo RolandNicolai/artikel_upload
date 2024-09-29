abd44bbe6498184768b2a40b18f374c19bdef25a
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 13:22:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import yaml
 
 hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 from yaml.loader import SafeLoader
-with open('../config.yaml') as file:
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = Authenticate(
