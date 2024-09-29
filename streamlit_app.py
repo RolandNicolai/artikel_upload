730fea71e65ca4c13f26b0be9e19c71c835742af
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 12:19:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,20 @@ from datetime import datetime
 import streamlit_authenticator as stauth
 
 
+hashed_passwords = stauth.Hasher(passwords).generate()
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
 
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if st.session_state['authentication_status']:
+    authenticator.logout('Logout', 'main')
+    st.write('Welcome *%s*' % (st.session_state['name']))
+    st.title('Some content')
+elif st.session_state['authentication_status'] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] == None:
+    st.warning('Please enter your username and password')
 
 
 LOGO_URL_LARGE = "https://bonnierpublications.com/app/themes/bonnierpublications/assets/img/logo.svg"
