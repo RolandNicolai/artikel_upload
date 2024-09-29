9ae57b5face378b06b259c9e251e19d9fa0e04f6
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 13:45:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import hmac
-import streamlit as st
 import time
 import pytz
 from datetime import datetime
+from google.oauth2 import service_account
+from google.cloud import storage
 
 
 def check_password():
@@ -71,3 +72,36 @@ elif 12 <= current_hour < 18:
     st.title(":orange[God eftermiddag] " + first_name.capitalize())
 else:
     st.title(":orange[Godaften] " + first_name.capitalize())
+
+
+
+# Setup Google Cloud credentials
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["vertexAI_service_account"]
+)
+
+def upload_blob(bucket_name, file_obj, destination_blob_name):
+    """Uploads a file to the bucket from a file-like object."""
+    storage_client = storage.Client(credentials=credentials)
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    
+    # Upload the file directly from the file-like object
+    blob.upload_from_file(file_obj, content_type="application/pdf")
+    
+    st.write(f"File uploaded to {bucket_name}/{destination_blob_name}.")
+
+# Streamlit file uploader
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+# File name in cloud storage
+destination_blob_name = "test1.pdf"
+
+# Handle file upload when button is clicked
+if st.button("Upload fil"):
+    if uploaded_file is not None:
+        # Call the upload_blob function with the file object
+        upload_blob("vertex_search_assets", uploaded_file, destination_blob_name)
+    else:
+        st.error("No file uploaded. Please upload a PDF file.")
+
