5f757c8b1ae35999f3cdcf98e2d1f974e87b9622
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 15:12:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
-st. title("Google Auth demo!")
+st.title("Google Auth demo!")
 
-if not st. experimental_user. is_logged_in():
+if not st.experimental_user.is_logged_in():
     st.warning("User not logged in")
 
-google_button = st. button ("Login with Google", type="primary")
+google_button = st.button ("Login with Google", type="primary")
 
 if google_button:
     st. experimental_user. login (provider="google")
 
 if st.experimental_user.is_logged_in():
-    st write(": sparkles: :rainbow[User data]")
+    st.write(": sparkles: :rainbow[User data]")
 
-st. write(st. experimental_user)
-logout_button = st. button ("Logout")
+st.write(st. experimental_user)
+logout_button = st.button ("Logout")
 
 if logout_button:
-    st. experimental_ user. logout()
+    st.experimental_ user.logout()
