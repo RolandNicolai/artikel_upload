9a26f9454fe670ebdf96c44ece8203c36bd962ef
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 15:12:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ if not st.experimental_user.is_logged_in():
 google_button = st.button ("Login with Google", type="primary")
 
 if google_button:
-    st. experimental_user. login (provider="google")
+    st. experimental_user.login (provider="google")
 
 if st.experimental_user.is_logged_in():
     st.write(": sparkles: :rainbow[User data]")
@@ -16,4 +16,4 @@ st.write(st. experimental_user)
 logout_button = st.button ("Logout")
 
 if logout_button:
-    st.experimental_ user.logout()
+    st.experimental_user.logout()
