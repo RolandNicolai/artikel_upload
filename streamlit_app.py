eea94ca068158193569c97d76b2c895c745d251c
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 16:43:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,32 @@ import pytz
 from datetime import datetime
 from google.oauth2 import service_account
 from google.cloud import storage
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query("select * from `bonnier-deliverables.vertexAI_search.token_table`")
+
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['token'])
 
 
 def check_password():
