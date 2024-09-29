468f8708ca2b000fc735373467e28d7bf0d1520e
RolandNicolai <nicolai.roland@bonnier.dk>
2024-09-29 12:33:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit_authenticator as stauth
 
 
 hashed_passwords = stauth.Hasher(st.secrets["credentials"]["passwords"]).generate()
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+authenticator = stauth.Authenticate(st.secrets["credentials"]["names"], st.secrets["credentials"]["usernames"], hashed_passwords,
     'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
 
 name, authentication_status, username = authenticator.login('Login', 'main')
